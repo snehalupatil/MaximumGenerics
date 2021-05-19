16e24472b6459060647864541a413e9b8e5ab85e
snehalupatil <patilsnehal4575@gmail.com>
2021-05-19 11:45:46 +0530

    Added: Given 3 Floats return Maximum

--- main/java/com/bl/demo/Max.java
+++ main/java/com/bl/demo/Max.java
@@ -14,8 +14,27 @@ public class Max {
         return max;
     }
 
-    public static void printMaximum(Integer m)
+    public static void printIntMaximum(Integer m)
     {
         System.out.println("Maximum Number is: "+m);
+    }
+
+    public static Float findMaximumFloat(Float a, Float b, Float c) {
+        Float max;
+        max = a;
+        if (b.compareTo(max) == 1)
+            max = b;
+        if (c.compareTo(max) == 1)
+            max = c;
+        printFloatMaximum(max);
+        return max;
+    }
+    public static void printFloatMaximum(Float m) {
+        System.out.println("Maximum float Number is: " + m);
+    }
+
+    public static void main(String[] args){
+        Max genericFloat = new Max();
+        genericFloat.findMaximumFloat(6.4f,2.2f,4.5f);
     }
 }
